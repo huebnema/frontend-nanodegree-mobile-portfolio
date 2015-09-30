61a720efd46534a6f056ff3c8364ada0278414aa
Maddie <maddie.huebner@gmail.com>
2015-09-30 14:45:19 -0500

    Test grunt install (seems buggy)

---
+++ node_modules/grunt/Gruntfile.js
@@ -0,0 +1,9 @@
+concat: {
+    dist: {
+        src: [
+            'js/*.js', // All JS in the libs folder
+            'views/js/*.js', // All JS in the libs folder
+        ],
+        dest: 'js/build/production.js',
+    }
+}
