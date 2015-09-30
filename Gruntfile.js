83742751b65784fb80be9da00475a92755cec5d8
Maddie <maddie.huebner@gmail.com>
2015-09-30 15:58:39 -0500

    Revert "Test grunt install (seems buggy)"
    This reverts commit 61a720efd46534a6f056ff3c8364ada0278414aa.

--- Gruntfile.js
+++
@@ -1,9 +0,0 @@
-concat: {
-    dist: {
-        src: [
-            'js/*.js', // All JS in the libs folder
-            'views/js/*.js', // All JS in the libs folder
-        ],
-        dest: 'js/build/production.js',
-    }
-}
