8175d794369a5f59d2882587b391774a63c4587f
Maddie <maddie.huebner@gmail.com>
2015-10-28 18:00:04 -0500

    Generate pizzas using Window.innerHeight

--- views/js/main.js
+++ views/js/main.js
@@ -544,18 +544,28 @@ window.addEventListener('scroll', updatePositions);
 
 // Reference for getting started:  https://discussions.udacity.com/t/can-anyone-provide-hints-or-tips-for-achieving-60-fps/4395/2
 
+// Reference for calculating number of pizzas with inner.Height:  https://discussions.udacity.com/t/calculating-number-of-pizzas-with-inner-height/35343/6
+
 // Generates the sliding pizzas when the page loads.
 document.addEventListener('DOMContentLoaded', function() {
   var cols = 8;
   var s = 256;
-  for (var i = 0; i < 20; i++) {
+  var rowTop = 0;
+
+  for (var i = 0; i < 200; i++) {
+    rowTop = (Math.floor(i / cols) * s);
+
+    if (rowTop > window.innerHeight) {
+      break;
+    }
+
     var elem = document.createElement('img');
     elem.className = 'mover';
     elem.src = "images/pizza.png";
     elem.style.height = "100px";
     elem.style.width = "73.333px";
     elem.basicLeft = (i % cols) * s;
-    elem.style.top = (Math.floor(i / cols) * s) + 'px';
+    elem.style.top = rowTop + 'px';
     document.querySelector("#movingPizzas1").appendChild(elem);
   }
   updatePositions();
